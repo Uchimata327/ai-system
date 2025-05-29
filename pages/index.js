2ce6965a990bf5428a2becb167a43ef1d9fb4724
Uchimata327 <kristoffersanton@gmail.com>
2025-05-29 22:29:12 +0000

    Add basic index page to fix 404

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
+      <h1>Velkommen til AI-system 👋</h1>
+      <p>Deployen virker! Du ser nå root-siden fra <code>pages/index.js</code>.</p>
+    </div>
+  );
+}
